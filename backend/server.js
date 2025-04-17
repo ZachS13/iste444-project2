4d849b91abdb30fe31df0cf3189fef0dd34494fa
zachs1326 <zachs1326@gmail.com>
2025-04-17 15:00:52 -0400

    started making the server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
 // Service layer
+const express = require('express'),
+      cors = require('cors'),
+      app = express(),
+      port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Successful connection!' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
