9024e93686f51923723b8f5491688ceba090e899
zachs1326 <zachs1326@gmail.com>
2025-04-17 16:03:29 -0400

    API endpoints are made, but don't connect to the business layer yet

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,49 @@ app.get('/', (req, res) => {
     res.json({ message: 'Successful connection!' });
 });
 
+// USER API ENDPOINTS
+app.get('/api/v1/users', (req, res) => {
+    res.json({ message: 'Returns a list of users' });
+});
+
+app.get('/api/v1/user/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `User with ID ${id}` });
+});
+
+app.post('/api/v1/user', (req, res) => {
+    const { username, password } = req.body;
+    res.json({ message: `User ${username} created successfully!` });
+});
+
+
+// BOOK API ENDPOINTS
+app.get('/api/v1/books', (req, res) => {
+    res.json({ message: 'Returns a list of books' });
+});
+
+app.get('/api/v1/book/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Book with ID ${id}` });
+});
+
+app.post('/api/v1/book', (req, res) => {
+    const { title, author, genre, publishedYear } = req.body;
+    res.json({ message: `Book ${title} by ${author} created successfully!` });
+});
+
+app.put('/api/v1/book/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, author, genre, publishedYear } = req.body;
+    res.json({ message: `Book with ID ${id} updated successfully!` });
+});
+
+app.delete('/api/v1/book/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Book with ID ${id} deleted successfully!` });
+});
+
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
